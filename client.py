a3db0d4033035c975264606678bc57c194d5ef7b
legocuber <legocuber@gmail.com>
2018-01-14 15:01:25 -0500

    client/server base
    client/server base, with commands, etc

---
+++ client.py
@@ -0,0 +1,109 @@
+import socket
+import time
+import ssl
+from tkinter import *
+from tkinter.scrolledtext import *
+from threading import Thread
+
+class Chat(Frame):
+	def addMessage(self, text):
+		self.messages.config(state=NORMAL)
+		self.messages.insert(END, text)
+		self.messages.config(state=DISABLED)
+		self.messages.see('end')
+	def sendMessageEvent(self, event):
+		self.sendMessage()
+	def sendMessage(self):
+		global s
+		text = self.entryText.get()
+		if len(text) > 0:
+			if text[0] == '/':
+				print(b'C=' + bytes(text[1:].encode('utf-8')))
+				s.send(b'C=' + bytes(text[1:].encode('utf-8')))
+			else:
+				s.send(b'M=' + bytes(text.encode('utf-8')))
+		self.entryText.set('')
+	def createWidgets(self):
+		
+		self.messages = ScrolledText(self, height=15, width=30)
+		self.messages.config(state=DISABLED)
+		self.messages.pack({'side':'right'})
+		
+		self.entryText = StringVar()
+		self.INP = Entry(self, textvariable = self.entryText)
+		self.INP.pack()
+		self.INP.bind('<Return>', self.sendMessageEvent)
+		
+		self.SEND = Button(self)
+		self.SEND['text'] = 'SEND'
+		self.SEND['fg'] = 'green'
+		self.SEND['command'] = self.sendMessage
+		self.SEND.pack({'side':'left'})
+		
+		self.QUIT = Button(self)
+		self.QUIT['text'] = 'QUIT'
+		self.QUIT['fg'] = 'red'
+		self.QUIT['command'] = self.quit
+		self.QUIT.pack({'side':'right'})
+		
+	def __init__(self, master=None):
+		Frame.__init__(self, master)
+		
+		self.pack()
+		self.createWidgets()
+
+name = 'bob'#input("Name:")
+host = '173.66.160.91'#input("Host [default 184.191.64.88]:")
+port = int(input("Port [default 25565]:") or 25565)
+
+s = ssl.wrap_socket(socket.socket(), ciphers='SHA1')
+
+s.settimeout(3)
+try:
+	s.connect((host, port))
+except:
+	print("Could not connect to server.")
+	
+connectionAlive = True
+
+s.send(bytes(name.encode('utf-8'))) # send name
+
+timeFormat = '{:%Y-%m-%d %H:%M:%S}'
+
+def receive():
+	global s, connectionAlive, chat
+	print(connectionAlive)
+	while connectionAlive:
+		try:
+			new = s.recv(1024).decode('utf-8') # received message
+			if new == '?=HB': # if heartbeat message:
+				time.sleep(0.2)
+				s.send(b'?=HB')
+			elif new == '?=QUIT':
+				connectionAlive = False
+				s.close()
+			elif new[:2] == 'M=':
+				chat.addMessage(new[2:] + "\n")
+		except:
+			connectionAlive = False
+			s.close()
+			try:
+				tk.destroy()
+			except: pass
+	print("Connection closed")
+
+receiverThread = Thread(target=receive)
+receiverThread.daemon = True
+receiverThread.start()
+
+tk = Tk(screenName='Chat')
+tk.geometry('400x400')
+chat = Chat(master=tk)
+chat.mainloop()
+try:
+	tk.destroy()
+except: pass
+connectionAlive = False
+
+s.close()
+input("Press enter to close.")
---
+++ server.py
@@ -0,0 +1,86 @@
+import socket
+import time
+import ssl
+import datetime
+from threading import Thread
+
+serverOn = True
+clients = []
+threads = []
+
+timeFormat = '{:%Y-%m-%d %H:%M:%S}'
+
+def spam(client, *message):
+	global clients
+	for x in range(10):
+		sendMsgAll(clients, client.name+' : '+' '.join(message))
+def birthday(client, *message):
+	global clients
+	sendMsgAll(clients, '%s: Happy birthday to you,\n%s: Happy birthday to you,\n%s: Happy birthday dear %s,\n%s: Happy birthday to you!' %(client.name, client.name, client.name, ' '.join(message), client.name))
+def ctime(client, *a):
+	client.socket.send(bytes(('M=[server] : '+timeFormat.format(datetime.datetime.now())).encode('utf-8')))
+commandMap = {'spam':spam, 'birthday':birthday, 'time':ctime}
+class Client:
+	def __init__(self, socket, addr):
+		global threads, clients
+		
+		self.socket = socket
+		self.addr = addr
+		self.name = socket.recv(1024).decode('utf-8') # receive a name
+		self.connectionAlive = True
+		self.thread = Thread(target=self.handle)
+		threads.append(self.thread)
+		self.thread.daemon = True
+		self.thread.start()
+		sendToAll(clients, '%s has joined the server!' % self.name)
+	def handle(self):
+		self.socket.send(b'?=HB')
+		global clients, commandMap
+		while self.connectionAlive:
+			#try:
+			new = self.socket.recv(1024).decode('utf-8')
+			if new == '?=HB':
+				time.sleep(0.2)
+				self.socket.send(b'?=HB')
+			elif new[:2] == 'M=':
+				print(addr,self.name,':',new[2:])
+				sendToAll(clients, "M=%s : %s" % (self.name, new[2:]))
+			elif new[:2] == 'C=':
+				command = new[2:]
+				print(addr,self.name,':',command)
+				csplit = command.split(' ')
+				function = csplit[0]
+				if function not in commandMap:
+					self.socket.send(b'M=[server] : That command does not exist')
+				elif len(csplit) > 1:
+					commandMap[function](self, *csplit[1:])
+				else:
+					commandMap[function](self)
+			'''except:
+				print("Connection closed:", self.addr)
+				self.connectionAlive = False
+				self.socket.close()
+				clients.remove(self)
+				sendToAll(clients, 'M=%s left the chat.' % self.name)'''
+def sendMsgAll(clients, message):
+	sendToAll(clients, 'M=' + message)
+def sendToAll(clients, message):
+	for client in clients:
+		client.socket.send(bytes(message.encode('utf-8')))
+s = ssl.wrap_socket(socket.socket(), ciphers='SHA1')
+host = socket.gethostname()
+port = int(input("Port[default 25565]:") or 25565)
+print("Your hostname:", host)
+
+s.bind((host, port))
+s.listen(5)
+
+while True:
+	c,addr = s.accept()
+	print("Got connection from",addr)
+	c.settimeout(3)
+	clients.append(Client(c,addr))
+sendToAll(clients, "?=QUIT")
+print("Everyone left, closing server...")
+s.close()
+input("Press enter to close.")
